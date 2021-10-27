b2ee893499b4a56d2fc15e1fb19f47d1d61c2437
Dinesh42 <dineshkumar05851@gmail.com>
2021-10-27 21:16:06 +0530

    main file is not considering anymore

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Main from "./Main/Main";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <Header />
     </div>
   );
 }
