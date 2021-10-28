7d4b40758cde7fef7fd3effba647767e5402c11a
Dinesh42 <dineshkumar05851@gmail.com>
2021-10-28 22:05:39 +0530

    header completed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,20 +5,72 @@ import Cart2 from "../../assests/image/cart-2.jpg";
 import Cart3 from "../../assests/image/cart-3.jpg";
 
 export default function Header() {
-  const [isSearch, setIsSearch] = useState(false);
-  const [isShoppingCart, setIsShoppingCart] = useState(false);
-
   const onSearchClick = () => {
-    setIsSearch(!isSearch);
+    let searchForm = document.querySelector(".search-form");
+    let shoppingCart = document.querySelector(".shopping-cart");
+    let login = document.querySelector(".login-form");
+    let navbar = document.querySelector(".navbar");
+    document.querySelector("#search-btn").onclick = () => {
+      searchForm.classList.toggle("active");
+      shoppingCart.classList.remove("active");
+      login.classList.remove("active");
+      navbar.classList.remove("active");
+    };
   };
   const onShoppingCart = () => {
-    setIsShoppingCart(!isShoppingCart);
+    let shoppingCart = document.querySelector(".shopping-cart");
+    let searchForm = document.querySelector(".search-form");
+    let login = document.querySelector(".login-form");
+    let navbar = document.querySelector(".navbar");
+    document.querySelector("#cart-btn").onclick = () => {
+      shoppingCart.classList.toggle("active");
+      searchForm.classList.remove("active");
+      login.classList.remove("active");
+      navbar.classList.remove("active");
+    };
+  };
+
+  const onLoginClick = () => {
+    let login = document.querySelector(".login-form");
+    let searchForm = document.querySelector(".search-form");
+    let shoppingCart = document.querySelector(".shopping-cart");
+    let navbar = document.querySelector(".navbar");
+    document.querySelector("#login-btn").onclick = () => {
+      login.classList.toggle("active");
+      searchForm.classList.remove("active");
+      shoppingCart.classList.remove("active");
+      navbar.classList.remove("active");
+    };
+  };
+
+  const onMenubar = () => {
+    let navbar = document.querySelector(".navbar");
+    let searchForm = document.querySelector(".search-form");
+    let shoppingCart = document.querySelector(".shopping-cart");
+    let login = document.querySelector(".login-form");
+    document.querySelector("#menu-bar").onclick = () => {
+      navbar.classList.toggle("active");
+      searchForm.classList.remove("active");
+      shoppingCart.classList.remove("active");
+      login.classList.remove("active");
+    };
+  };
+
+  window.onscroll = () => {
+    let searchForm = document.querySelector(".search-form");
+    let shoppingCart = document.querySelector(".shopping-cart");
+    let login = document.querySelector(".login-form");
+    let navbar = document.querySelector(".navbar");
+    searchForm.classList.remove("active");
+    shoppingCart.classList.remove("active");
+    login.classList.remove("active");
+    navbar.classList.remove("active");
   };
 
   return (
     <header className="header">
       <a href="/" className="logo">
-        <i class="fas fa-shopping-basket"></i> Grocery
+        <i className="fas fa-shopping-basket"></i> Grocery
       </a>
       <nav className="navbar">
         <a href="/">Home</a>
@@ -29,68 +81,100 @@ export default function Header() {
         <a href="/">Contact</a>
       </nav>
       <div className="icons">
-        <div id="menu-bar" className="fas fa-bars"></div>
         <div
-          id="search-bar"
+          id="menu-bar"
+          className="fas fa-bars"
+          onClick={() => onMenubar()}
+        ></div>
+        <div
+          id="search-btn"
           className="fas fa-search"
           onClick={() => onSearchClick()}
         ></div>
         <div
-          id="cart-bar"
+          id="cart-btn"
           className="fas fa-shopping-cart"
+          onClick={() => onShoppingCart()}
+        ></div>
+        <div
+          id="login-btn"
+          className="fas fa-user"
           onClick={() => {
-            onShoppingCart();
+            onLoginClick();
           }}
         ></div>
-        <div id="login-btn" className="fas fa-user"></div>
       </div>
 
-      {isSearch && (
-        <form action="" className="search-form">
-          <input type="search" placeholder="search here..." id="search-box" />
-          <label id="seach-box" className="fas fa-search"></label>
-        </form>
-      )}
-      {isShoppingCart && (
-        <div className="shopping-cart">
-          <div className="box">
-            <i className="fas fa-times"></i>
-            <img src={Cart1} alt="" />
-            <div className="content">
-              <h3>Organic Food</h3>
-              <span className="quantity">1</span>
-              <span className="multiply">X</span>
-              <span className="price">$10.58</span>
-            </div>
+      <form action="" className="search-form">
+        <input type="search" placeholder="search here..." id="search-box" />
+        <label id="seach-box" className="fas fa-search"></label>
+      </form>
+      <div className="shopping-cart">
+        <div className="box">
+          <i className="fas fa-times"></i>
+          <img src={Cart1} alt="" />
+          <div className="content">
+            <h3>Organic Food</h3>
+            <span className="quantity">1</span>
+            <span className="multiply">X</span>
+            <span className="price">$10.58</span>
           </div>
-          <div className="box">
-            <i className="fas fa-times"></i>
-            <img src={Cart2} alt="" />
-            <div className="content">
-              <h3>Organic Food</h3>
-              <span className="quantity">1</span>
-              <span className="multiply">X</span>
-              <span className="price">$10.58</span>
-            </div>
-          </div>
-          <div className="box">
-            <i className="fas fa-times"></i>
-            <img src={Cart3} alt="" />
-            <div className="content">
-              <h3>Organic Food</h3>
-              <span className="quantity">1</span>
-              <span className="multiply">X</span>
-              <span className="price">$10.58</span>
-            </div>
-          </div>
-          <h3 className="total">
-            total: <span>$31.74</span>
-          </h3>
-          <a href="" className="btn">
-            checkout cart
-          </a>
         </div>
-      )}
+        <div className="box">
+          <i className="fas fa-times"></i>
+          <img src={Cart2} alt="" />
+          <div className="content">
+            <h3>Organic Food</h3>
+            <span className="quantity">1</span>
+            <span className="multiply">X</span>
+            <span className="price">$10.58</span>
+          </div>
+        </div>
+        <div className="box">
+          <i className="fas fa-times"></i>
+          <img src={Cart3} alt="" />
+          <div className="content">
+            <h3>Organic Food</h3>
+            <span className="quantity">1</span>
+            <span className="multiply">X</span>
+            <span className="price">$10.58</span>
+          </div>
+        </div>
+        <h3 className="total">
+          total: <span>$31.74</span>
+        </h3>
+        <a href="" className="btn">
+          checkout cart
+        </a>
+      </div>
+      <form action="" className="login-form">
+        <h3>Login From</h3>
+        <input
+          type="email"
+          name="name"
+          ie=""
+          placeholder="Enter your email"
+          className="box"
+        />
+        <input
+          type="password"
+          name="password"
+          ie=""
+          placeholder="Enter password"
+          className="box"
+        />
+        <div className="remember">
+          <input type="checkbox" name="" id="remember-me" />
+          <label if="remember-me">remember me</label>
+        </div>
+        <input type="submit" value="login now" className="btn" />
+        <p>
+          forget password <a href="/">click here</a>
+        </p>
+        <p>
+          don't have an account<a href="/">create one</a>
+        </p>
+      </form>
     </header>
   );
 }
