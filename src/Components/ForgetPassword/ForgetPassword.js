724c31a18d396a5c8fa6bb13ef8441eb499edf7d
Dinesh42 <dineshkumar05851@gmail.com>
2021-12-29 20:26:05 +0530

    3 files committed

--- src/Components/ForgetPassword/ForgetPassword.js
+++ src/Components/ForgetPassword/ForgetPassword.js
@@ -1,64 +1,64 @@
-import React from "react";
+// import React from "react";
 
-export default function ForgetPassword() {
-  const oncloseFrgtBtn = () => {
-    let forget = document.querySelector(".forget-password");
-    forget.classList.remove("active");
-  };
+// export default function ForgetPassword() {
+//   const oncloseFrgtBtn = () => {
+//     let forget = document.querySelector(".forget-password");
+//     forget.classList.remove("active");
+//   };
 
-  const onLoginClick = () => {
-    let login = document.querySelector(".login-form");
-    let searchForm = document.querySelector(".search-form");
-    let shoppingCart = document.querySelector(".shopping-cart");
-    let navbar = document.querySelector(".navbar");
-    login.classList.toggle("active");
-    let forget = document.querySelector(".forget-password");
-    let signup = document.querySelector(".signup-form");
-    searchForm.classList.remove("active");
-    shoppingCart.classList.remove("active");
-    navbar.classList.remove("active");
-    signup.classList.remove("active");
-    forget.classList.remove("active");
-  };
+//   const onLoginClick = () => {
+//     let login = document.querySelector(".login-form");
+//     let searchForm = document.querySelector(".search-form");
+//     let shoppingCart = document.querySelector(".shopping-cart");
+//     let navbar = document.querySelector(".navbar");
+//     login.classList.toggle("active");
+//     let forget = document.querySelector(".forget-password");
+//     let signup = document.querySelector(".signup-form");
+//     searchForm.classList.remove("active");
+//     shoppingCart.classList.remove("active");
+//     navbar.classList.remove("active");
+//     signup.classList.remove("active");
+//     forget.classList.remove("active");
+//   };
 
-  return (
-    <div>
-      <form action="" className="forget-password" id="forget-password">
-        <span>
-          <h3>Forget Password</h3>
-          <i
-            className="fas fa-times"
-            onClick={() => {
-              oncloseFrgtBtn();
-            }}
-          ></i>
-        </span>
-        <input
-          type="email"
-          name="name"
-          ie=""
-          placeholder="Username"
-          className="box"
-        />
-        <input
-          type="password"
-          name="password"
-          ie=""
-          placeholder="New Password"
-          className="box"
-        />
-        <input
-          type="password"
-          name="password"
-          ie=""
-          placeholder="Confirm password"
-          className="box"
-        />
-        <input type="submit" value="forget passord" className="btn" />
-        <p>
-          <span onClick={() => onLoginClick()}>Go back to login</span>
-        </p>
-      </form>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <form action="" className="forget-password">
+//         <span>
+//           <h3>Forget Password</h3>
+//           <i
+//             className="fas fa-times"
+//             onClick={() => {
+//               oncloseFrgtBtn();
+//             }}
+//           ></i>
+//         </span>
+//         <input
+//           type="email"
+//           name="name"
+//           ie=""
+//           placeholder="Username"
+//           className="box"
+//         />
+//         <input
+//           type="password"
+//           name="password"
+//           ie=""
+//           placeholder="New Password"
+//           className="box"
+//         />
+//         <input
+//           type="password"
+//           name="password"
+//           ie=""
+//           placeholder="Confirm password"
+//           className="box"
+//         />
+//         <input type="submit" value="forget passord" className="btn" />
+//         <p>
+//           <span onClick={() => onLoginClick()}>Go back to login</span>
+//         </p>
+//       </form>
+//     </div>
+//   );
+// }
